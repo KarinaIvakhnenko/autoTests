97200f6a5c720249577e9094ab9de9267e122c89
Karina Ivakhnenko <ivakhnenkokarina111@gmail.com>
2021-09-20 12:22:08 +0300

    5 try to set chromedriver for macOS

--- src/test/java/com/google/WebDriverSettings.java
+++ src/test/java/com/google/WebDriverSettings.java
@@ -1,17 +1,19 @@
 package com.google;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
 
 public class WebDriverSettings {
-    public ChromeDriver driver;
+    public static ChromeDriver driver;
     public String value;
 
-    @Before
-    public void setUp() throws IOException {
+    @BeforeClass
+    public static void setUp() throws IOException {
 
         if (System.getProperty("os.name").toLowerCase().contains("mac os x")) {
             Runtime.getRuntime().exec("chmod 755 " + ".//chromedriver");
@@ -19,13 +21,14 @@ public class WebDriverSettings {
 
         if(System.getProperty("os.name").toLowerCase().contains("windows")){
             System.setProperty("webdriver.chrome.driver", ".//chromedriver.exe");
+
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
         }
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
     }
 
-    @After
-    public void close(){
+    @AfterClass
+    public static void close(){
         driver.quit();
     }
 }
