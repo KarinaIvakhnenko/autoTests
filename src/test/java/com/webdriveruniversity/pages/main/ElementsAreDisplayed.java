dbac5e1d1fbd9420ffe6a0cc23760e997a9376e5
KarinaIvakhnenko <ivakhnenkokarina111@gmail.com>
2021-09-29 17:51:54 +0300

    Tests for checking that elements are displayed on main page

--- src/test/java/com/webdriveruniversity/pages/main/ElementsAreDisplayed.java
+++ src/test/java/com/webdriveruniversity/pages/main/ElementsAreDisplayed.java
@@ -5,11 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import com.webdriveruniversity.resources.SwitchTabs;
+
 
 public class ElementsAreDisplayed extends WebDriverSettings {
     public static final String HTTPS_WEBDRIVERUNIVERSITY_COM = "https://webdriveruniversity.com";
+
+    SwitchTabs sTabs = new SwitchTabs();
 
     @Before
     public void setUp() {
@@ -23,103 +25,110 @@ public class ElementsAreDisplayed extends WebDriverSettings {
     }
 
     @Test
-    public void section_ContactUs(){
+    public void section_ContactUs() {
         driver.findElement(By.xpath("//a[@id='contact-us']//h1[text()='CONTACT US']")).isDisplayed();
     }
 
     @Test
-    public void section_LoginPortal(){
+    public void section_LoginPortal() {
         driver.findElement(By.xpath("//a[@id='login-portal']//h1[text()='LOGIN PORTAL']")).isDisplayed();
     }
 
     @Test
-    public void section_ButtonsClicks(){
+    public void section_ButtonsClicks() {
         driver.findElement(By.xpath("//a[@id='button-clicks']//h1[text()='BUTTON CLICKS']")).isDisplayed();
     }
 
     @Test
-    public void section_ToDoList(){
+    public void section_ToDoList() {
         driver.findElement(By.xpath("//a[@id='to-do-list']//h1[text()='TO DO LIST']")).isDisplayed();
     }
 
     @Test
-    public void section_PageObjectModel(){
+    public void section_PageObjectModel() {
         driver.findElement(By.xpath("//a[@id='page-object-model']//h1[text()='PAGE OBJECT MODEL']")).isDisplayed();
     }
 
     @Test
-    public void section_AccordionText(){
+    public void section_AccordionText() {
         driver.findElement(By.xpath("//a[@id='page-object-model']//h1[text()='ACCORDION & TEXT AFFECTS (APPEAR & DISAPPEAR)']")).isDisplayed();
     }
 
     @Test
-    public void section_DropdownCheckboxe(){
+    public void section_DropdownCheckboxe() {
         driver.findElement(By.xpath("//a[@id='dropdown-checkboxes-radiobuttons']//h1[text()='DROPDOWN, CHECKBOXE(S) & RADIO BUTTON(S)']")).isDisplayed();
     }
 
     @Test
-    public void section_AjaxLoader(){
+    public void section_AjaxLoader() {
         driver.findElement(By.xpath("//a[@id='ajax-loader']//h1[text()='AJAX LOADER']")).isDisplayed();
     }
 
     @Test
-    public void section_Actions(){
+    public void section_Actions() {
         driver.findElement(By.xpath("//a[@id='actions']//h1[text()='ACTIONS']")).isDisplayed();
     }
 
     @Test
-    public void section_ScrollingAround(){
+    public void section_ScrollingAround() {
         driver.findElement(By.xpath("//a[@id='scrolling-around']//h1[text()='SCROLLING AROUND']")).isDisplayed();
     }
 
     @Test
-    public void section_PopupAlerts(){
+    public void section_PopupAlerts() {
         driver.findElement(By.xpath("//a[@id='popup-alerts']//h1[text()='POPUP & ALERTS']")).isDisplayed();
     }
 
     @Test
-    public void section_Iframe(){
+    public void section_Iframe() {
         driver.findElement(By.xpath("//a[@id='iframe']//h1[text()='IFRAME']")).isDisplayed();
     }
 
     @Test
-    public void section_HiddenElements(){
+    public void section_HiddenElements() {
         driver.findElement(By.xpath("//a[@id='hidden-elements']//h1[text()='HIDDEN ELEMENTS']")).isDisplayed();
     }
 
     @Test
-    public void section_DataTables(){
+    public void section_DataTables() {
         driver.findElement(By.xpath("//a[@id='data-table']//h1[text()='DATA, TABLES & BUTTON STATES']")).isDisplayed();
     }
 
     @Test
-    public void section_AutocompleteTextfield(){
+    public void section_AutocompleteTextfield() {
         driver.findElement(By.xpath("//a[@id='autocomplete-textfield']//h1[text()='AUTOCOMPLETE TEXTFIELD']")).isDisplayed();
     }
 
     @Test
-    public void section_FileUpload(){
+    public void section_FileUpload() {
         driver.findElement(By.xpath("//a[@id='file-upload']//h1[text()='FILE UPLOAD']")).isDisplayed();
     }
 
     @Test
-    public void section_Datepicker(){
+    public void section_Datepicker() {
         driver.findElement(By.xpath("//a[@id='datepicker']//h1[text()='DATEPICKER']")).isDisplayed();
     }
 
+    @Test
+    public void udemyPromoCode1() {
+        driver.findElement(By.xpath("//img[@id='udemy-promo-code-webdriverio']")).isDisplayed();
 
+    }
 
+    @Test
+    public void udemyPromoCode2() {
+        driver.findElement(By.xpath("//img[@id='udemy-promo-code-cucumber']")).isDisplayed();
 
+    }
 
-
-
-
-
-
-
-
-
-
+    @Test
+    public void click_OnContactUs() {
+        String linkContactUs = "https://webdriveruniversity.com/Contact-Us/contactus.html";
+        driver.findElement(By.xpath("//div[@class='col-sm-12 col-lg-8 col-md-8']")).click();
+        sTabs.switchTabs(linkContactUs);
+        String linkCurrent = driver.getCurrentUrl();
+        Assert.assertEquals(linkCurrent, linkContactUs);
+    }
 
 
 //    @Test
---
+++ src/test/java/com/webdriveruniversity/resources/SwitchTabs.java
@@ -0,0 +1,68 @@
+package com.webdriveruniversity.resources;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.devtools.idealized.Javascript;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class SwitchTabs extends WebDriverSettings {
+
+//    public void switchTabs(String link){
+//
+////        ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+////        driver.switchTo().window(tabs2.get(1));
+////        driver.close();
+////        driver.switchTo().window(tabs2.get(0));
+//
+////    protected void changeWindow(int i){
+////        Set<String> windows = driver.getWindowHandles();
+////        String[] handle = windows.toArray(new String[windows.size()]);
+////        driver.switchTo().window(handle[i]);
+////        driver.switchTo().window(handle[i]).getTitle();
+////    }
+
+    public void switchTabs (String link) {
+        Set<String> handles = driver.getWindowHandles();
+        String current = driver.getWindowHandle();
+        if (handles.size() > 1)
+        {
+            handles.remove(current);
+            String newTab = handles.iterator().next();
+            driver.switchTo().window(newTab);
+        }
+
+
+//        JavascriptExecutor js = (JavascriptExecutor)driver;
+//
+//        try{
+//
+//            String window1 = driver.getWindowHandle();
+//
+//            js.executeScript("window.open()");
+//
+//            Set<String> currentWindows = driver.getWindowHandles();
+//
+//            String window2 = null;
+//
+//            for (String window : currentWindows){
+//                if (!window.equals(window1)){
+//                    window2 = window;
+//                    break;
+//                }
+//            };
+//            driver.switchTo().window(window2);
+//            //driver.getCurrentUrl();
+//            //driver.close();
+//            //driver.switchTo().window(window1);
+//        }
+//        //catch (InterruptedException e){
+//           // e.printStackTrace();
+//        //}
+//        finally {
+//            driver.quit();
+//        }
+    }
+}
