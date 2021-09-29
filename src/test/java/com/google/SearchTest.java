69166dd707038678e157e09e5dc30b62f8ae4ead
KarinaIvakhnenko <ivakhnenkokarina111@gmail.com>
2021-09-29 19:22:46 +0300

    Added static import for assert

--- src/test/java/com/google/SearchTest.java
+++ src/test/java/com/google/SearchTest.java
@@ -1,12 +1,13 @@
 package com.google;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import resources.WebDriverSettings;
+
+import static org.junit.Assert.*;
 
 
 public class SearchTest extends WebDriverSettings {
@@ -21,14 +22,14 @@ public class SearchTest extends WebDriverSettings {
     @Test
     public void testTitlePage() {
         String title = driver.getTitle();
-        Assert.assertEquals("Google", title);
+        assertEquals("Google", title);
     }
 
     @Test
     public void testInputInSearchBox() {
         driver.findElement(By.name("q")).sendKeys("allo.ua");
         String qElementValue = driver.findElement(By.name("q")).getAttribute("value");
-        Assert.assertEquals("allo.ua", qElementValue);
+        assertEquals("allo.ua", qElementValue);
 
     }
 
@@ -38,14 +39,14 @@ public class SearchTest extends WebDriverSettings {
         driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']")).click();
 
         String currentLink = driver.getCurrentUrl();
-        Assert.assertTrue(currentLink.contains("q=allo.ua"));
+        assertTrue(currentLink.contains("q=allo.ua"));
     }
 
     @Test
     public void testLinkIsDisplayed() {
         driver.findElement(By.name("q")).sendKeys("allo.ua" + Keys.ENTER);
         WebElement linkAllo = driver.findElement(By.xpath("//div[@class='yuRUbf']//a[@href='https://allo.ua/']"));
-        Assert.assertTrue(linkAllo.isDisplayed());
+        assertTrue(linkAllo.isDisplayed());
     }
 
     @Test
@@ -53,6 +54,6 @@ public class SearchTest extends WebDriverSettings {
         driver.findElement(By.name("q")).sendKeys("allo" + Keys.ENTER);
         driver.findElement(By.xpath("//div[@class='yuRUbf']//a[@href='https://allo.ua/']")).click();
         String link = driver.getCurrentUrl();
-        Assert.assertEquals("https://allo.ua/", link);
+        assertEquals("https://allo.ua/", link);
     }
 }
